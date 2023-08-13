f3707b35a8fb32046d2606cc2b1793b2112b8c07
puzika <tolibjonlatipovk@gmail.com>
2023-08-13 19:50:38 +0700

    Added a 'testimonials' section

--- src/index.js
+++ src/index.js
@@ -6,15 +6,24 @@ import headerImg from './assets/hero.png';
 import featuresImg1 from './assets/digital.jpg';
 import featuresImg2 from './assets/grow.jpg';
 import featuresImg3 from './assets/card.jpg';
+import userImg1 from './assets/user-1.jpg';
+import userImg2 from './assets/user-2.jpg';
+import userImg3 from './assets/user-3.jpg';
 
 const logoNavigation = document.querySelector('.navigation__logo');
 const headerImage = document.querySelector('.header__image');
 const featuresImage1 = document.querySelector('.features__image--1');
 const featuresImage2 = document.querySelector('.features__image--2');
 const featuresImage3 = document.querySelector('.features__image--3');
+const userImage1 = document.querySelector('.user__image--1');
+const userImage2 = document.querySelector('.user__image--2');
+const userImage3 = document.querySelector('.user__image--3');
 
 logoNavigation.src = logo;
 headerImage.src = headerImg;
 featuresImage1.src = featuresImg1;
 featuresImage2.src = featuresImg2;
 featuresImage3.src = featuresImg3;
+userImage1.src = userImg1;
+userImage2.src = userImg2;
+userImage3.src = userImg3;
